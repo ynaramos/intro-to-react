7b8e35b683b7f2619b89f58e7681ff84943c1dbb
Yna Ramos <yna.francesca.ramos@gmail.com>
2021-02-13 23:29:49 +0000

    Change board render from being hardcoded to for loop

--- intro-app/src/index.js
+++ intro-app/src/index.js
@@ -21,25 +21,21 @@ class Board extends React.Component {
     }
   
     render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
+        const rowNum = 3;
+        const board = [];
+
+        for (let j = 0; j < rowNum; j++) {
+            const row = [];
+            for (let k = 0; k < rowNum; k++) {
+                row.push(this.renderSquare(k + (3 * j)))
+            }
+
+            board.push(<div className="board-row">{row}</div>)
+        }
+
+        return (
+            <div>{board}</div>
+        );
     }
  }
   
